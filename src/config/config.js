d2ad36ba85219ca2cfeca80ab43b75cb32817fb1
itsrusty <rusty3031@gmail.com>
2023-10-25 19:59:01 -0700

    upload config database and others configs

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,20 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
 
-const connection_db = mongoose.connection({
-   
-})
+dotenv.config()
+const MONGO_URI = process.env.MONGO_URI
+// console.log(MONGO_URI)
 
-export default connection_db
+const connection = async () => {
+    try {
+        await mongoose.connect(MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log("[database connected]".bgBlue)
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+export default connection
