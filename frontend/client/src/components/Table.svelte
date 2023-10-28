082dd5661f1426f875e067b9c77c19fddbecfeaf
itsrusty <rusty3031@gmail.com>
2023-10-28 13:32:30 -0700

    deploy test

--- frontend/client/src/components/Table.svelte
+++ frontend/client/src/components/Table.svelte
@@ -1,15 +1,14 @@
-<script>
-  import "../app.css";
+<script lang="ts">
   import { onMount } from "svelte";
-  
-  let data;
 
-  onMount(()=> {
-    
-  })
+  let dataStudent: any[] = [];
+  onMount(async () => {
+    const response = await fetch("https://school-api-lrj2.onrender.com/api/students");
+    const data = await response.json();
+    dataStudent = data.data;
+  });
 </script>
 
-<!-- component -->
 <body class="antialiased font-sans bg-gray-200">
   <div class="container mx-auto px-4 sm:px-8">
     <div class="py-8">
@@ -18,262 +17,75 @@
       </div>
       <div class="my-2 flex sm:flex-row flex-col">
         <div class="flex flex-row mb-1 sm:mb-0">
+          <a href="/admin" type="button">Añadir nuevo estudiante</a>
           <div class="relative">
-            <select
-              class="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            >
-              <option>5</option>
-              <option>10</option>
-              <option>20</option>
-            </select>
-            <div
-              class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-            >
-              <svg
-                class="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                />
-              </svg>
-            </div>
-          </div>
-          <div class="relative">
-            <div
-              class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-            >
-              <svg
-                class="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                />
+            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
               </svg>
             </div>
           </div>
         </div>
-        <!-- Base -->
       </div>
       <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
         <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
           <table class="min-w-full leading-normal">
             <thead>
               <tr>
-                <th
-                  class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-                >
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                   Nombre completo
                 </th>
-                <th
-                  class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-                >
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                   Curso
                 </th>
-                <th
-                  class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-                >
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                   Matrícula
                 </th>
-                <th
-                  class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-                >
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                   Número Cel
                 </th>
-                <th
-                  class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-                >
+                <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                   Número Documento
                 </th>
               </tr>
             </thead>
 
             <!-- data here -->
-
             <tbody>
-              <tr>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <div class="flex items-center">
-                    <div class="flex-shrink-0 w-10 h-10">
-                      <img
-                        class="w-full h-full rounded-full"
-                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                        alt=""
-                      />
+              {#each dataStudent as student}
+                <tr>
+                  <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <div class="flex items-center">
+                      <div class="flex-shrink-0 w-10 h-10">
+                        <img class="w-full h-full rounded-full" src="https://via.placeholder.com/150" alt="" />
+                      </div>
+                      <div class="ml-3">
+                        <p class="text-gray-900 whitespace-no-wrap">
+                          {student.firstName} {student.lastName}
+                        </p>
+                      </div>
                     </div>
-                    <div class="ml-3">
-                      <p class="text-gray-900 whitespace-no-wrap">
-                        Vera Carpenter
-                      </p>
-                    </div>
-                  </div>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">Matematicas</p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">7216278616</p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <span
-                    class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
-                  >
-                    <span
-                      aria-hidden
-                      class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-                    />
-                    <span class="relative">6334096902</span>
-                  </span>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <span
-                    class="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight"
-                  >
-                    <span
-                      aria-hidden
-                      class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
-                    />
-                    <span class="relative">1627812631923126</span>
-                  </span>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <div class="flex items-center">
-                    <div class="flex-shrink-0 w-10 h-10">
-                      <img
-                        class="w-full h-full rounded-full"
-                        src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                        alt=""
-                      />
-                    </div>
-                    <div class="ml-3">
-                      <p class="text-gray-900 whitespace-no-wrap">
-                        Blake Bowman
-                      </p>
-                    </div>
-                  </div>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">Programación</p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">38902813789</p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <span
-                    class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
-                  >
-                    <span
-                      aria-hidden
-                      class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-                    />
-                    <span class="relative">82932890318</span>
-                  </span>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <span
-                    class="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight"
-                  >
-                    <span
-                      aria-hidden
-                      class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
-                    />
-                    <span class="relative">8903827</span>
-                  </span>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <div class="flex items-center">
-                    <div class="flex-shrink-0 w-10 h-10">
-                      <img
-                        class="w-full h-full rounded-full"
-                        src="https://images.unsplash.com/photo-1540845511934-7721dd7adec3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                        alt=""
-                      />
-                    </div>
-                    <div class="ml-3">
-                      <p class="text-gray-900 whitespace-no-wrap">Dana Moore</p>
-                    </div>
-                  </div>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">Ciencias basicas</p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">389017382372</p>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <span
-                    class="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight"
-                  >
-                    <span
-                      aria-hidden
-                      class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
-                    />
-                    <span class="relative">3782782639</span>
-                  </span>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <span
-                    class="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight"
-                  >
-                    <span
-                      aria-hidden
-                      class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
-                    />
-                    <span class="relative">92019213789</span>
-                  </span>
-                </td>
-              </tr>
-              <tr>
-                <td class="px-5 py-5 bg-white text-sm">
-                  <div class="flex items-center">
-                    <div class="flex-shrink-0 w-10 h-10">
-                      <img
-                        class="w-full h-full rounded-full"
-                        src="https://images.unsplash.com/photo-1522609925277-66fea332c575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&h=160&w=160&q=80"
-                        alt=""
-                      />
-                    </div>
-                    <div class="ml-3">
-                      <p class="text-gray-900 whitespace-no-wrap">Alonzo Cox</p>
-                    </div>
-                  </div>
-                </td>
-                <td class="px-5 py-5 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">Finanzas</p>
-                </td>
-                <td class="px-5 py-5 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">21238930280</p>
-                </td>
-                <td class="px-5 py-5 bg-white text-sm">
-                  <span
-                    class="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight"
-                  >
-                    <span
-                      aria-hidden
-                      class="absolute inset-0 bg-red-200 opacity-50 rounded-full"
-                    />
-                    <span class="relative">8291308908</span>
-                  </span>
-                </td>
-                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <span
-                    class="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight"
-                  >
-                    <span
-                      aria-hidden
-                      class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
-                    />
-                    <span class="relative">3672352673</span>
-                  </span>
-                </td>
-              </tr>
+                  </td>
+                  <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <p class="text-gray-900 whitespace-no-wrap">{student.Course}</p>
+                  </td>
+                  <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <p class="text-gray-900 whitespace-no-wrap">{student.NumberMat}</p>
+                  </td>
+                  <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                      <span aria-hidden class="absolute inset-0 bg-green-200 opacity-50 rounded-full" />
+                      <span class="relative">{student.NumberPhone}</span>
+                    </span>
+                  </td>
+                  <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <span class="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight">
+                      <span aria-hidden class="absolute inset-0 bg-orange-200 opacity-50 rounded-full" />
+                      <span class="relative">{student.NumberDoc}</span>
+                    </span>
+                  </td>
+                </tr>
+              {/each}
             </tbody>
           </table>
         </div>
--- frontend/client/src/routes/admin/+page.svelte
+++ frontend/client/src/routes/admin/+page.svelte
@@ -1,88 +1,124 @@
 <script>
   import "../../app.css";
+  let firstName = "";
+  let lastName = "";
+  let Course = "";
+  let NumberMat = "";
+  let NumberPhone = "";
+  let NumberDoc = "";
 
+  async function handleSubmit() {
+    const url = "https://school-api-lrj2.onrender.com/api/students/new"; // URL de la API
+    const data = {
+      firstName,
+      lastName,
+      Course,
+      NumberMat,
+      NumberPhone,
+      NumberDoc,
+    };
 
-  const handleCreate = async () => {
-    // cuando se registre regresar al index
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+      const message = `An error has occurred: ${res.status}`;
+      console.error(message);
+      // Agrega aquí la lógica para manejar el error de acuerdo a tus necesidades
+    } else {
+      const result = await res.json();
+      console.log(result); // Puedes manejar la respuesta de la API de acuerdo a tus necesidades
+    }
   }
 </script>
 
-<!-- component -->
-<!-- Create by joker banny -->
 <div class="h-screen bg-indigo-100 flex justify-center items-center">
   <div class="lg:w-2/5 md:w-1/2 w-2/3">
-    <form class="bg-white p-10 rounded-lg shadow-lg min-w-full">
+    <form
+      class="bg-white p-10 rounded-lg shadow-lg min-w-full"
+      on:submit|preventDefault={handleSubmit}
+    >
       <h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">
         Registrar estudiante
       </h1>
       <div>
         <label
           class="text-gray-800 font-semibold block my-3 text-md"
-          for="username">Nombre completo</label
+          for="firstName">Nombre completo</label
         >
         <input
           class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
           type="text"
-          name="username"
-          id="username"
+          name="firstName"
+          id="firstName"
+          bind:value={firstName}
           placeholder="Nombre completo"
         />
       </div>
       <div>
         <label
           class="text-gray-800 font-semibold block my-3 text-md"
-          for="email">Curso</label
+          for="lastName">Curso</label
         >
         <input
           class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
           type="text"
-          name="email"
-          id="email"
+          name="lastName"
+          id="lastName"
+          bind:value={Course}
           placeholder="Sistemas"
         />
       </div>
       <div>
         <label
           class="text-gray-800 font-semibold block my-3 text-md"
-          for="password">Matrícula</label
+          for="Course">Matrícula</label
         >
         <input
           class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
           type="text"
-          name="password"
-          id="password"
+          name="Course"
+          id="Course"
+          bind:value={NumberMat}
           placeholder="3783678362"
         />
       </div>
       <div>
         <label
           class="text-gray-800 font-semibold block my-3 text-md"
-          for="confirm">Número de télefono</label
+          for="NumberPhone">Número de télefono</label
         >
         <input
           class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
           type="text"
-          name="confirm"
-          id="confirm"
+          name="NumberPhone"
+          id="NumberPhone"
+          bind:value={NumberPhone}
         />
       </div>
       <div>
         <label
           class="text-gray-800 font-semibold block my-3 text-md"
-          for="confirm">Número de documento</label
+          for="NumberDoc">Número de documento</label
         >
         <input
           class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
           type="text"
-          name="confirm"
-          id="confirm"
+          name="NumberDoc"
+          id="NumberDoc"
+          bind:value={NumberDoc}
           placeholder="9023208989"
         />
-      </div><br>
+      </div>
+      <br />
       <button
         type="submit"
         class="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans"
-        on:click={handleCreate}
         >Crear</button
       >
     </form>
