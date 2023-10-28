1074509a8fb37921120d267aa58c4583a8f15c3a
itsrusty <rusty3031@gmail.com>
2023-10-28 12:12:14 -0700

    deploy api

--- frontend/client/src/components/Table.svelte
+++ frontend/client/src/components/Table.svelte
@@ -1,11 +1,12 @@
 <script>
   import "../app.css";
+  import { onMount } from "svelte";
+  
+  let data;
 
-  // get data
-  const getData = () => {};
-
-  // create user
-  const createStudent = () => {};
+  onMount(()=> {
+    
+  })
 </script>
 
 <!-- component -->
@@ -40,13 +41,6 @@
             </div>
           </div>
           <div class="relative">
-            <select
-              class="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500"
-            >
-              <option>Todos</option>
-              <option>Active</option>
-              <option>Inactive</option>
-            </select>
             <div
               class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
             >
@@ -62,7 +56,7 @@
             </div>
           </div>
         </div>
-        <button on:click={createStudent}>jksjk</button>
+        <!-- Base -->
       </div>
       <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
         <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
@@ -98,7 +92,7 @@
             </thead>
 
             <!-- data here -->
-            
+
             <tbody>
               <tr>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -118,10 +112,10 @@
                   </div>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">Admin</p>
+                  <p class="text-gray-900 whitespace-no-wrap">Matematicas</p>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">Jan 21, 2020</p>
+                  <p class="text-gray-900 whitespace-no-wrap">7216278616</p>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                   <span
@@ -131,7 +125,7 @@
                       aria-hidden
                       class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
                     />
-                    <span class="relative">Activo</span>
+                    <span class="relative">6334096902</span>
                   </span>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -142,7 +136,7 @@
                       aria-hidden
                       class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
                     />
-                    <span class="relative">Suspended</span>
+                    <span class="relative">1627812631923126</span>
                   </span>
                 </td>
               </tr>
@@ -164,10 +158,10 @@
                   </div>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">Editor</p>
+                  <p class="text-gray-900 whitespace-no-wrap">Programación</p>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">Jan 01, 2020</p>
+                  <p class="text-gray-900 whitespace-no-wrap">38902813789</p>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                   <span
@@ -177,7 +171,7 @@
                       aria-hidden
                       class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
                     />
-                    <span class="relative">Activo</span>
+                    <span class="relative">82932890318</span>
                   </span>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -188,7 +182,7 @@
                       aria-hidden
                       class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
                     />
-                    <span class="relative">Suspended</span>
+                    <span class="relative">8903827</span>
                   </span>
                 </td>
               </tr>
@@ -208,10 +202,10 @@
                   </div>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">Editor</p>
+                  <p class="text-gray-900 whitespace-no-wrap">Ciencias basicas</p>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">Jan 10, 2020</p>
+                  <p class="text-gray-900 whitespace-no-wrap">389017382372</p>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                   <span
@@ -221,7 +215,7 @@
                       aria-hidden
                       class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
                     />
-                    <span class="relative">Suspended</span>
+                    <span class="relative">3782782639</span>
                   </span>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -232,7 +226,7 @@
                       aria-hidden
                       class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
                     />
-                    <span class="relative">Suspended</span>
+                    <span class="relative">92019213789</span>
                   </span>
                 </td>
               </tr>
@@ -252,10 +246,10 @@
                   </div>
                 </td>
                 <td class="px-5 py-5 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">Admin</p>
+                  <p class="text-gray-900 whitespace-no-wrap">Finanzas</p>
                 </td>
                 <td class="px-5 py-5 bg-white text-sm">
-                  <p class="text-gray-900 whitespace-no-wrap">Jan 18, 2020</p>
+                  <p class="text-gray-900 whitespace-no-wrap">21238930280</p>
                 </td>
                 <td class="px-5 py-5 bg-white text-sm">
                   <span
@@ -265,7 +259,7 @@
                       aria-hidden
                       class="absolute inset-0 bg-red-200 opacity-50 rounded-full"
                     />
-                    <span class="relative">Inactive</span>
+                    <span class="relative">8291308908</span>
                   </span>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -276,31 +270,12 @@
                       aria-hidden
                       class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
                     />
-                    <span class="relative">Suspended</span>
+                    <span class="relative">3672352673</span>
                   </span>
                 </td>
               </tr>
             </tbody>
           </table>
-          <div
-            class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between"
-          >
-            <span class="text-xs xs:text-sm text-gray-900">
-              Showing 1 to 4 of 50 Entries
-            </span>
-            <div class="inline-flex mt-2 xs:mt-0">
-              <button
-                class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l"
-              >
-                Prev
-              </button>
-              <button
-                class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r"
-              >
-                Next
-              </button>
-            </div>
-          </div>
         </div>
       </div>
     </div>
---
+++ frontend/client/src/routes/admin/+page.svelte
@@ -0,0 +1,90 @@
+<script>
+  import "../../app.css";
+
+
+  const handleCreate = async () => {
+    // cuando se registre regresar al index
+  }
+</script>
+
+<!-- component -->
+<!-- Create by joker banny -->
+<div class="h-screen bg-indigo-100 flex justify-center items-center">
+  <div class="lg:w-2/5 md:w-1/2 w-2/3">
+    <form class="bg-white p-10 rounded-lg shadow-lg min-w-full">
+      <h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">
+        Registrar estudiante
+      </h1>
+      <div>
+        <label
+          class="text-gray-800 font-semibold block my-3 text-md"
+          for="username">Nombre completo</label
+        >
+        <input
+          class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Nombre completo"
+        />
+      </div>
+      <div>
+        <label
+          class="text-gray-800 font-semibold block my-3 text-md"
+          for="email">Curso</label
+        >
+        <input
+          class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Sistemas"
+        />
+      </div>
+      <div>
+        <label
+          class="text-gray-800 font-semibold block my-3 text-md"
+          for="password">Matrícula</label
+        >
+        <input
+          class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+          type="text"
+          name="password"
+          id="password"
+          placeholder="3783678362"
+        />
+      </div>
+      <div>
+        <label
+          class="text-gray-800 font-semibold block my-3 text-md"
+          for="confirm">Número de télefono</label
+        >
+        <input
+          class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+          type="text"
+          name="confirm"
+          id="confirm"
+        />
+      </div>
+      <div>
+        <label
+          class="text-gray-800 font-semibold block my-3 text-md"
+          for="confirm">Número de documento</label
+        >
+        <input
+          class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+          type="text"
+          name="confirm"
+          id="confirm"
+          placeholder="9023208989"
+        />
+      </div><br>
+      <button
+        type="submit"
+        class="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans"
+        on:click={handleCreate}
+        >Crear</button
+      >
+    </form>
+  </div>
+</div>
